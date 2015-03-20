1cfc0d2706d926a49ddc056631d8de9b3fa921fe
Sönke Hahn <soenke.hahn@zalora.com>
2015-03-20 11:43:38 +0800

    nix: update default.nix

--- default.nix
+++ default.nix
@@ -1,25 +1,24 @@
-{ pkgs ? import <nixpkgs> {},
+{
+  pkgs ?
+    let
+      bootstrap_pkgs = import <nixpkgs> {};
+      nixpkgsGit = bootstrap_pkgs.fetchgit {
+        url = "https://github.com/nixos/nixpkgs";
+        rev = "5f7d37480d7fb31d1850652739dd66d41e8f25d9";
+        sha256 = "1x00xnd4y4mi8ddzh2kb66k9m4r9hfhnls5y92ny287ckd2n9dl3";
+      };
+    in import nixpkgsGit {},
   src ? builtins.filterSource (path: type:
     type != "unknown" &&
     baseNameOf path != ".git" &&
     baseNameOf path != "result" &&
     baseNameOf path != "dist" &&
     baseNameOf path != ".nhc") ./.
-}:
-let
-  hdevtoolsSrc = pkgs.fetchgit {
-    url = "https://github.com/schell/hdevtools.git";
-    rev = "4ff36c55ed64a774067697d9830a490bb9028263";
-    sha256 = "612946494aef28a0a9bf0e48b7e93f878f02791ec0735019ee49ae6e4eb04daf";
-  };
-  hdevtools = pkgs.haskellPackages.buildLocalCabal hdevtoolsSrc "hdevtools";
-in
-pkgs.haskellPackages.buildLocalCabalWithArgs {
+}: pkgs.haskellPackages.buildLocalCabalWithArgs {
   inherit src;
   name = "cron-syslog-viewer";
   cabalDrvArgs = {
     buildTools = [
-      hdevtools
       pkgs.gnuplot
     ];
   };
